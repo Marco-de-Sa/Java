3805bb2e8e883a80d410d989bdfd8fe9b0e65753
Marco <128144743+Marco-de-Sa@users.noreply.github.com>
2023-12-05 15:15:57 +0000

    Add files via upload

---
+++ lesson3/src/exercise/ShoppingCart.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exercise;
+
+/**
+ *
+ * @author gaurd
+ */
+public class ShoppingCart {
+    public static void main(String[]Args){
+        System.out.println("Hello world!");
+    }
+}
