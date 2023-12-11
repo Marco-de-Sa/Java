a79a8fa10c6463b34afa6c1c9fba1c921ef684de
Marco <128144743+Marco-de-Sa@users.noreply.github.com>
2023-12-11 10:56:21 +0000

    Add files via upload

---
+++ lesson6/src/exercise1/Item.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exercise1;
+
+/**
+ *
+ * @author gaurd
+ */
+public class Item {
+
+    public int itemID;
+    public String desc;
+    public double price;
+    public int quantity;
+}
---
+++ lesson6/src/exercise2/ShoppingCart.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exercise2;
+
+/**
+ *
+ * @author gaurd
+ */
+public class ShoppingCart {
+    public static void main(String[] args) {
+        
+    }
+}
