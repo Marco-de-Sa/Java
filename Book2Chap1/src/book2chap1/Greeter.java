6d2bba949d3a182e2abb33d2a6ad651f7fb6fe2d
Marco <128144743+Marco-de-Sa@users.noreply.github.com>
2024-01-24 11:37:45 +0000

    Add files via upload

---
+++ Book2Chap1/src/book2chap1/Greeter.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package book2chap1;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author gaurd
+ */
+public class Greeter {
+    public void sayHello(){
+        JOptionPane.showMessageDialog(null, "Hello, world", "Greeter", JOptionPane.INFORMATION_MESSAGE);
+    }
+}
---
+++ Book2Chap1/src/book2chap1/HelloApp.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package book2chap1;
+
+/**
+ *
+ * @author gaurd
+ */
+public class HelloApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Greeter myGreeterObject = new Greeter();
+        myGreeterObject.sayHello();
+    }
+    
+}
