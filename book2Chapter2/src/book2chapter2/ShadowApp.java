88cdebc565fa214c3af12817f6b4545d26c37ace
Marco <128144743+Marco-de-Sa@users.noreply.github.com>
2024-01-24 11:38:21 +0000

    Add files via upload

---
+++ book2Chapter2/src/book2chapter2/ShadowApp.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package book2chapter2;
+
+/**
+ *
+ * @author gaurd
+ */
+public class ShadowApp {
+
+    /**
+     * @param args the command line arguments
+     */
+//    static int x;
+//
+//    public static void main(String[] args) {
+//        x = 5;
+//        System.out.println("x = " + x);
+//        int x;
+//        x = 10;
+//        System.out.println("x = " + x);
+//        System.out.println("ShadowApp.x = " + ShadowApp.x);
+//    }
+
+}
